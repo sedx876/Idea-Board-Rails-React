af59f0898b9a5f6c6a6fae9eb219e3aa84f39006
sedx876 <swatkins876@gmail.com>
2020-08-23 09:50:43 -0400

    IdeaForm component

--- ideaboard/src/components/IdeasContainer.js
+++ ideaboard/src/components/IdeasContainer.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Idea from './Idea'
+import update from 'immutability-helper'
+import IdeaForm from './IdeaForm'
 
 class IdeasContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      ideas: []
+      ideas: [],
+      editingIdeaId: null
     }
+  }
+
+  addNewIdea = () => {
+    axios.post(
+      'http://localhost:3001/api/v1/ideas',
+      { idea:
+        {
+          title: '',
+          body: ''
+        }
+      }
+    )
+    .then(response => {
+      console.log(response)
+      const ideas = update(this.state.ideas, {
+        $splice: [[0, 0, response.data]]
+      })
+      this.setState({ideas: ideas, editingIdeaId: response.data.id})
+    })
+    .catch(error => console.log(error))
+  }
+
+  updateIdea = (idea) => {
+    const ideaIndex = this.state.ideas.findIndex(x => x.id === idea.id)
+    const ideas = update(this.state.ideas, {
+      [ideaIndex]: { $set: idea }
+    })
+    this.setState({ideas: ideas})
   }
 
   componentDidMount() {
@@ -22,9 +53,18 @@ class IdeasContainer extends Component {
   render() {
     return (
       <div>
-        {this.state.ideas.map((idea) => {
+      <button className="newIdeaButton"
+        onClick={this.addNewIdea}>
+        New Idea
+      </button>
+      {this.state.ideas.map((idea) => {
+      if(this.state.editingIdeaId === idea.id) {
+        return(<IdeaForm idea={idea} key={idea.id}
+                updateIdea={this.updateIdea} />)
+      } else {
         return (<Idea idea={idea} key={idea.id} />)
-        })}
+    }
+  })}
       </div>
     );
   }
